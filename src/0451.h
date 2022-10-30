77feb866258fd283e98d22ae8f9b837f81860ff2
zixiao_bios <zixiao_bios@outlook.com>
2022-10-30 12:52:01 +0800

    可以去坡峰岭玩了捏 ~(￣▽￣)~*

--- src/0451.h
+++ src/0451.h
@@ -1,56 +1,54 @@
 #ifndef LEETCODE_0451_H
 #define LEETCODE_0451_H
 
-#include <unordered_map>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
-    void quickSort(vector<pair<char, int>> &count, int start, int end) {
-        if (end <= start) {
+    void quickSort(vector<pair<char, int>> &data, int start, int end) {
+        if (start >= end) {
             return;
         }
 
-        swap(count[start], count[(rand() % (end - start + 1)) + start]);
         int l = start, r = end;
-        pair<char, int> key = count[l];
+        auto key = data[l];
 
         while (l < r) {
-            while (l < r and count[r].second <= key.second) {
+            while (l < r and data[r].second <= key.second) {
                 --r;
             }
-            count[l] = count[r];
+            data[l] = data[r];
 
-            while (l < r and count[l].second >= key.second) {
+            while (l < r and data[l].second >= key.second) {
                 ++l;
             }
-            count[r] = count[l];
+            data[r] = data[l];
         }
+        data[l] = key;
 
-        count[r] = key;
-        quickSort(count, start, r - 1);
-        quickSort(count, r + 1, end);
+        quickSort(data, start, l - 1);
+        quickSort(data, l + 1, end);
     }
 
     string frequencySort(string s) {
-        unordered_map<char, int> countMap;
-        for (auto &ch: s) {
-            ++countMap[ch];
+        unordered_map<char, int> count;
+        for (auto &each: s) {
+            ++count[each];
         }
 
-        vector<pair<char, int>> count(countMap.size());
-        int i = 0;
-        for (auto &each: countMap) {
-            count[i++] = each;
-        }
-
-        quickSort(count, 0, count.size() - 1);
-
-        string ans = "";
+        vector<pair<char, int>> times(count.size());
         for (auto &each: count) {
-            for (int j = 0; j < each.second; ++j) {
+            times.emplace_back(each);
+        }
+
+        quickSort(times, 0, times.size() - 1);
+
+        string ans;
+        for (auto &each: times) {
+            for (int i = 0; i < each.second; ++i) {
                 ans += each.first;
             }
         }
