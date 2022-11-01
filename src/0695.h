5eec12da55030731607887695796ff62fc476f5d
zixiao_bios <zixiao_bios@outlook.com>
2022-11-01 23:55:37 +0800

    dfs时，要先判断边界，再判断业务逻辑

--- src/0695.h
+++ src/0695.h
@@ -7,32 +7,39 @@ using namespace std;
 
 class Solution {
 public:
+    vector<pair<int, int>> direction = {{0,  1},
+                                        {0,  -1},
+                                        {1,  0},
+                                        {-1, 0}};
+
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        if (grid.empty() or grid[0].empty()) {
-            return 0;
-        }
+        vector<vector<bool>> flag(grid.size(), vector<bool>(grid[0].size(), false));
 
         int ans = 0;
-
         for (int i = 0; i < grid.size(); ++i) {
             for (int j = 0; j < grid[0].size(); ++j) {
-                if (grid[i][j] == 1) {
-                    ans = max(ans, dfs(grid, i, j));
-                }
+                ans = max(ans, dfs(grid, flag, i, j));
             }
         }
 
         return ans;
     }
 
-    int dfs(vector<vector<int>> &grid, int i, int j) {
-        if (i < 0 or i >= grid.size() or j < 0 or j >= grid[0].size() or grid[i][j] == 0) {
+    int dfs(const vector<vector<int>> &grid, vector<vector<bool>> &flag, int i, int j) {
+        if (i < 0 or i >= grid.size() or j < 0 or j >= grid[0].size()) {
             return 0;
         }
 
-        grid[i][j] = 0;
+        if (flag[i][j] or grid[i][j] == 0) {
+            return 0;
+        }
 
-        return 1 + dfs(grid, i, j + 1) + dfs(grid, i, j - 1) + dfs(grid, i + 1, j) + dfs(grid, i - 1, j);
+        flag[i][j] = true;
+        int area = 0;
+        for (auto &each: direction) {
+            area += dfs(grid, flag, i + each.first, j + each.second);
+        }
+        return area + 1;
     }
 };
 
